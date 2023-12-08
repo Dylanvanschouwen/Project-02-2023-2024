bc6dc884532c407e2b8709b3f8e4518d335e442a
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2023-12-08 09:41:44 +0100

    made and prepared all files

--- Include/Navbar.php
+++ Include/Navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar that'll be on every page -->
     <nav>
-        <a href="Index.html">Home</a>
-        <a href="Goedendoelen.html">Goeden doelen</a>
-        <a href="Games.html">Games</a>
-        <a href="Portfolio.html">Portfolio</a>
+        <a href="Index.php">Home</a>
+        <a href="links-goedendoelen.php">Goeden doelen</a>
+        <a href="links-games.php">Games</a>
+        <a href="links-portfolio.php">Portfolio</a>
     </nav>
--- Webpages/links-portfolio.php
+++ Webpages/links-portfolio.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Stylesheet.css">
-    <title>Games</title>
+    <title>links naar portfolios</title>
     <link rel="stylesheet" href="stylesheet.css">
     <?php 
     include ('../Include/navbar.php'); 
@@ -14,3 +13,4 @@
 <body>
     
 </body>
+</html>
