4851b775b80e4cc6a903a4d76163cd732515cc29
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2024-01-10 14:50:01 +0100

    Pull deze versie

--- Webpages/Index.php
+++ Webpages/Index.php
@@ -3,34 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Your Gaming Website</title>
-    <link rel="stylesheet" href="s.css">
     <title>homepage game website</title>
-    <link rel="stylesheet" href="stylesheet.css">
+        <link rel="stylesheet" href="stylesheet.css">
     <?php 
     include ('../Include/navbar.php');
     include ('../Include/footer.php');
     ?>
 </head>
 <body>
-    <div id="HPS"></div>
     <h1>Our games</h1>
-    <img src="img./team-trees.png" alt="Roald game">
-    <img src="img./team-trees.png" alt="Marlon game">
-    <img src="img./team-trees.png" alt="Dylan game">
-    <br>
+        <img src="img./team-trees.png" alt="Roald game">
+        <img src="img./team-trees.png" alt="Marlon game">
+        <img src="img./team-trees.png" alt="Dylan game"> <br>
     <a href="uitleg game R">Game explanation</a>
     <a href="uitleg game M">Game explanation</a>
     <a href="uitleg game D">Game explanation</a>
     <br><br><br><br><br><br>
     <h1>About us</h1>
     <p>we're students at the TCR college in Rotterdam, our assignment <br>
-    for this period was to make a game in javascript for charity.</p>
-    <br>
-    <img src="" alt="Roald">
-    <img src="" alt="Marlon">
-    <img src="" alt="Dylan">
-    <p>you can view our portfolio </p><a href="portfolio.html">here.</a>
+    for this period was to make a game in javascript for charity.</p> <br>
+        <img src="" alt="Roald">
+        <img src="" alt="Marlon">
+        <img src="" alt="Dylan">
+    <p>you can view our portfolio </p>
+    <a href="portfolio.html">here.</a>
     <br><br><br><br><br><br>
     <h1>Donate to play</h1>
     <p>we're students at the TCR college in Rotterdam, our assignment <br>
--- Webpages/links-goedendoelen.php
+++ Webpages/links-goedendoelen.php
@@ -11,22 +11,20 @@
     ?>
 </head>
 <body> 
-<h1 class = gdh1>Our Charity</h1>
-<div class="container">
-    <div class="goeden-doelen">
-        <div class="1">
-            <img src="img/team-trees.png" class="pic" alt="team-trees">
-            <div class="marlon"><p>Marlon's Charity</p></div>
-        </div>
-        <div class="2">
-            <img src="img/team-trees.png" class="pic" alt="team-trees">
-            <div class="dylan"><p>Dylan's Charity</p></div>
-        </div>
-        <div class="3">
-            <img src="img/team-trees.png" class="pic" alt="team-trees">
-            <div class="roald"><p>Roald's Charity</p></div>
-        </div>
-    </div>
-</div>
+<!-- Main code -->
+<main id="klachtpage_main">
+    <header id="klachtpage_header">
+        <h1>Heeft u een klacht of compliment? Dien deze in met de formulieren hieronder!</h1>
+    </header>
+
+    <section>
+        <ul id="klachtpage_list">
+            <li><a href="goedendoelen-Dylan.php">Goedendoelen van Dylan</a></li>
+            <li><a href="goedendoelen-marlon.php">Goedendoelen van Marlon</a></li>
+            <li><a href="goedendoelen-Roald.php">Goedendoelen van Roald</a></li>
+        </ul>
+    </section>
+</main>
+
 </body>
 </html>
