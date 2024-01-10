0fad50bd5890574395aa7dc68ee2134567a725e1
RoaldTCR <9007433@student.zadkine.nl>
2024-01-10 14:30:34 +0100

    update

--- Webpages/Index.php
+++ Webpages/Index.php
@@ -24,6 +24,7 @@
     <a href="uitleg game D">Game explanation</a>
     <br><br><br><br><br><br>
     <h1>About us</h1>
+    <h1>TEST</h1>
     <p>we're students at the TCR college in Rotterdam, our assignment <br>
     for this period was to make a game in javascript for charity.</p>
     <br>
