b9be8567c60e0f9fd68d6c4e3fbc5c3fa45c15ab
RoaldTCR <9007433@student.zadkine.nl>
2024-01-21 20:16:27 +0100

    update

--- Webpages/Goedendoelen-Roald.php
+++ Webpages/Goedendoelen-Roald.php
@@ -9,8 +9,12 @@
 </head>
 <body>
     <main> 
-
-    
+    <p>I chose for team seas because I think protecting the oceans is important.</p>
+    <br>
+    <p>Team Seas is a collaborative environmental initiative launched by popular creators MrBeast and Mark Rober. This global movement aims to combat ocean pollution by removing 30 million pounds of trash from the world's oceans by the end of 2022. Through the power of community support and fundraising efforts, Team Seas works with various organizations and cleanup projects to make a positive impact on our planet's oceans, striving for a cleaner and healthier marine environment. Joining Team Seas means contributing to a collective effort to address the urgent issue of ocean pollution and protect the Earth's vital ecosystems.</p>
+    <br>
+    <img src="../Images/beast.png" alt="mr beast">
+    <br>
     </main>
     <?php include ('../Include/footer.php'); ?>
 </body>
--- Webpages/Motivatie-Roald.php
+++ Webpages/Motivatie-Roald.php
@@ -9,7 +9,7 @@
 </head>
 <body>
     <main> 
-
+    
     
     </main>
     <?php include ('../Include/footer.php'); ?>
