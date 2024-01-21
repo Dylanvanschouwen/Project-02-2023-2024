10f458bb06856eb1abc8a965415ab68ce5c68616
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2024-01-21 17:08:30 +0100

    .

--- Webpages/Game-uitleg-Dylan.php
+++ Webpages/Game-uitleg-Dylan.php
@@ -3,14 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Game uitleg tic-tac-toe</title>
     <link rel="stylesheet" href="stylesheet.css">
     <?php include ('../Include/navbar.php'); ?>
 </head>
 <body>
     <main> 
-
-    
+    <section class="Uitleg-D-game">
+        
+    </section>
     </main>
     <?php include ('../Include/footer.php'); ?>
 </body>
