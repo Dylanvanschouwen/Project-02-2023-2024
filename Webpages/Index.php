a39109856e97c02c989a22776c7c613476c6a252
RoaldTCR <9007433@student.zadkine.nl>
2024-01-21 22:11:51 +0100

    test

--- Webpages/Index.php
+++ Webpages/Index.php
@@ -31,8 +31,16 @@
     <a href="portfolio.html">here.</a>
     <br><br><br><br><br><br>
     <h1>Donate to play</h1>
-    <p>we're students at the TCR college in Rotterdam, our assignment <br>
+    <p>"Donate to play" is a phrase that can be used in the context of fundraising or charitable initiatives related to gaming or online content creation. It typically refers to a scenario where individuals or viewers can make a donation to a cause or charity in exchange for the opportunity to play a game or participate in an event.
+
+For example, a streamer on a platform like Twitch might organize a charity livestream where viewers can donate to a chosen charity. In return, the donors may have the chance to play a game with the streamer, receive special in-game items, or participate in other interactive activities during the stream.
+
+The idea is to encourage donations by offering some form of engagement or interaction as a reward. This approach is often used by content creators to leverage their platform and audience for charitable causes, making the act of donating more engaging and enjoyable for the contributors.</p>
+   <br> 
+<p>we're students at the TCR college in Rotterdam, our assignment <br>
     for this period was to make a game in javascript for charity.</p>
+    <br>
+    <br>
     </main>
 </body>
 <?php include ('../Include/footer.php'); ?>
