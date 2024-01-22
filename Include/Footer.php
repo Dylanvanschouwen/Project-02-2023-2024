420f9fbb6a3744b9eb37310f153ef05b93a86675
Dylan van schouwen <Dylanvanschouwen@gmail.com>
2024-01-22 04:14:25 +0100

    Finished everything but Dylan Portfolio, Re-added comments

--- Include/Footer.php
+++ Include/Footer.php
@@ -1,7 +1,5 @@
 <!-- Footer that'll be on every page -->
 <footer>
-<div class="footer-links">
-    
-    </div>
-    <p>&copy; 2024 Dylan, Roald en Marlon's website. All rights reserved by us (and a little chatgpt).</p>
+  <!-- Copyright information -->
+  <p>&copy; 2024 Dylan, Roald en Marlon's website. All rights reserved by us (and a little chatgpt).</p>
 </footer>
--- Include/Navbar.php
+++ Include/Navbar.php
@@ -1,7 +1,8 @@
 <!-- Navbar that'll be on every page -->
-    <nav>
-        <a href="Index.php">Home</a>
-        <a href="links-goedendoelen.php">Charities</a>
-        <a href="links-games.php">Games</a>
-        <a href="links-portfolio.php">Portfolio</a>
-    </nav>
+<nav>
+    <!-- Navigation links -->
+    <a href="Index.php">Home</a>
+    <a href="links-goedendoelen.php">Charities</a>
+    <a href="links-games.php">Games</a>
+    <a href="links-portfolio.php">Portfolio</a>
+</nav>
